8ca0587f1689a42e6c6c678e5df904f109d6f2b3
Dinara Abdukhamitova <231559@astanait.edu.kz>
2024-04-26 00:26:54 +0500

    node

---
+++ src/com/company/Node.java
@@ -0,0 +1,12 @@
+package com.company;
+    public class Node<T> {
+        T data;
+        com.company.Node next;
+
+        public Node(T data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+
