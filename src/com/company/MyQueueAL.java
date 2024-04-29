8464366ea845044661de2b94a777c976a0e3ad40
Dinara Abdukhamitova <231559@astanait.edu.kz>
2024-04-29 07:44:18 +0500

    assignment2

---
+++ src/com/company/MyQueueAL.java
@@ -0,0 +1,31 @@
+package com.company;
+
+public class MyQueueAL<E> {
+    private MyArrayList<E> list;
+
+    public MyQueueAL() {
+        list = new MyArrayList<>();
+    }
+
+    public void enqueue(E element) {
+        list.add(element);
+    }
+
+    public void dequeue() {
+        if(isEmpty()) {
+            throw  new IllegalStateException("Queue is empty");
+        }
+        list.remove(0);
+    }
+
+    public E peek() {
+        if(isEmpty()) {
+            throw  new IllegalStateException("Queue is empty");
+        }
+        return list.getElem(0);
+    }
+
+    public boolean isEmpty() {
+        return list.size() == 0;
+    }
+}
---
+++ src/com/company/MyStackAL.java
@@ -0,0 +1,30 @@
+package com.company;
+public class MyStackAL<E> {
+    private MyArrayList<E> list;
+
+    public MyStackAL() {
+        list = new MyArrayList<>();
+    }
+
+    public void push(E element) {
+        list.add(element);
+    }
+
+    public E peek() {
+        if(isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return list.getElem(list.size() - 1);
+    }
+
+    public void pop() {
+        if(isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        list.remove(list.size() - 1);
+    }
+
+    public boolean isEmpty() {
+        return list.size() == 0;
+    }
+}
